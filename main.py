832589ed829fcc35e20b9d8b2e313c6b07777705
manvillej <jeffmanvillejr@gmail.com>
2017-11-04 16:26:12 -0400

    Updated Docstring and code to PEP8

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pandas as pd
+# Compression Module
 import assignmentCompression as compression
 
 
 def main():
-    input("\nGet Started? ")
+    """"""
 
-    # get dataset
+    # get raw dataset
     df = pd.read_csv('./ExampleDataset.csv')
 
-    compressedDF = compression.compress(df, 0)
+    # call the compression algorithm
+    compressedDF = compression.compress(df)
 
     print(compressedDF.shape)
 
     # Save file results
     filename = input("What would you like to save this file as? :")
-    compressedDF.to_csv('{}.csv'.format(filename),index=False)
+    compressedDF.to_csv('{}.csv'.format(filename), index=False)
 
 
 if __name__ == '__main__':
